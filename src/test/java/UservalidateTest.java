3bc3d9e6e3fae00f1fa9ef44582f1ebe76ab65bd
MURA199918 <muralisaranr18@gmail.com>
2020-11-25 17:37:27 +0530

    added UC10

--- src/test/java/UservalidateTest.java
+++ src/test/java/UservalidateTest.java
@@ -4,37 +4,72 @@ import org.junit.Test;
 
 public class UservalidateTest {
     @Test
-    public void FirstName_Check(){
+    public void FirstName_Check_istrue(){
         UserValue validate=new UserValue();
         boolean result=validate.FirstName("Murali");
         Assert.assertEquals(true,result);
     }
 
     @Test
-    public void LastName_Check(){
+    public void FirstName_Check_isfalse(){
         UserValue validate=new UserValue();
-        boolean result=validate.LastName("hello");
+        boolean result=validate.FirstName("mu");
+        Assert.assertEquals(false,result);
+    }
+
+    @Test
+    public void LastName_Check_istrue(){
+        UserValue validate=new UserValue();
+        boolean result=validate.LastName("Hello");
         Assert.assertEquals(true,result);
     }
 
     @Test
-    public void Email_Check(){
+    public void LastName_Check_isfalse(){
+        UserValue validate=new UserValue();
+        boolean result=validate.LastName("he");
+        Assert.assertEquals(false,"he");
+    }
+
+    @Test
+    public void Email_Check_istrue(){
         UserValue validate=new UserValue();
         boolean result=validate.Email("murali@example.com");
         Assert.assertEquals(true,result);
     }
 
     @Test
-    public void Mobile_Check(){
+    public void Email_Check_isfalse(){
+        UserValue validate=new UserValue();
+        boolean result=validate.Email(".asx");
+        Assert.assertEquals(false,result);
+    }
+
+    @Test
+    public void Mobile_Check_istrue(){
         UserValue validate=new UserValue();
         boolean result=validate.Mobile(" 91 9894119421");
         Assert.assertEquals(true,result);
     }
 
     @Test
-    public void Password_Check(){
+    public void Mobile_Check_isfalse(){
+        UserValue validate=new UserValue();
+        boolean result=validate.Mobile("5793");
+        Assert.assertEquals(false,result);
+    }
+
+    @Test
+    public void Password_Check_istrue(){
         UserValue validate=new UserValue();
         boolean result=validate.Password("Muralis@18");
         Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void Password_Check_isfalse(){
+        UserValue validate=new UserValue();
+        boolean result=validate.Password("345");
+        Assert.assertEquals(false,result);
     }
 }
