58dea7869d360245dbea6a62259f6decde18bfcb
MURA199918 <muralisaranr18@gmail.com>
2020-11-26 16:46:08 +0530

    added UC12

--- src/main/java/JunitPracticeproblem/UserValue.java
+++ src/main/java/JunitPracticeproblem/UserValue.java
@@ -9,6 +9,10 @@ public class UserValue {
         this.message=message;
         return FirstName();
     }
+    public boolean LastName(String message) throws UserValueException{
+        this.message=message;
+        return LastName();
+    }
     public boolean FirstName() throws UserValueException {
         try {
             if (message.matches("^[A-Z][A-Z a-z]{3,25}$")) {
@@ -22,48 +26,65 @@ public class UserValue {
             throw new UserValueException("Please enter valid first name");
         }
     }
-/*
-    public boolean LastName(String lname) {
-        if(lname.matches("^[A-Z][A-Z a-z]{3,25}$")) {
-            System.out.println("Valid last name");
-            return true;
+
+    public boolean LastName() throws UserValueException {
+        try{
+            if(message.matches("^[A-Z][A-Z a-z]{3,25}$")) {
+                System.out.println("Valid last name");
+                return true;
+            }
+            else {
+                System.out.println("Invalid last name");
+                return false;
+            }
+        }catch (NullPointerException e){
+            throw new UserValueException("Please enter valid last name");
         }
-        else {
-            System.out.println("Invalid last name");
-            return false;
+
+    }
+
+    public boolean Email(Object o) throws UserValueException {
+        try {
+            if (message.matches("^[a-zA-Z]+([._+-]{0,1}[a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}+(?:\\.[a-z]{2,}){0,1}$")) {
+                System.out.println("valid email address");
+                return true;
+            } else {
+                System.out.println("Invalid email address");
+                return false;
+            }
+        }catch (NullPointerException e){
+            throw new UserValueException("Please enter valid emailid");
         }
     }
 
-    public boolean Email(String email) {
-        if(email.matches("^[a-zA-Z]+([._+-]{0,1}[a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}+(?:\\.[a-z]{2,}){0,1}$")) {
-            System.out.println("valid email address");
-            return true;
+    public boolean Mobile(Object o) throws UserValueException {
+        try{
+            if(message.matches("[91 ]{3}[789]{1}\\d{9}")) {
+                System.out.println("Valid phone number");
+                return true;
+            }
+            else {
+                System.out.println("Invalid phone number");
+                return false;
+            }
+        }catch (NullPointerException e){
+            throw new UserValueException("Please enter valid phone number");
         }
-        else {
-            System.out.println("Invalid email address");
-            return false;
-        }
+
     }
 
-    public boolean Mobile(String phone) {
-        if(phone.matches("[91 ]{3}[789]{1}\\d{9}")) {
-            System.out.println("Valid phone number");
-            return true;
-        }
-        else {
-            System.out.println("Invalid phone number");
-            return false;
+    public boolean Password(Object o) throws UserValueException {
+        try{
+            if(message.matches("^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#&$]).{8,}$")) {
+                System.out.println("Valid password");
+                return true;
+            }
+            else {
+                System.out.println("Invalid password");
+                return false;
+            }
+        }catch (NullPointerException e){
+            throw new UserValueException("Please enter valid password");
         }
     }
-
-    public boolean Password(String password) {
-        if(password.matches("^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#&$]).{8,}$")) {
-            System.out.println("Valid password");
-            return true;
-        }
-        else {
-            System.out.println("Invalid password");
-            return false;
-        }
-    }*/
 }
--- src/test/java/UservalidateTest.java
+++ src/test/java/UservalidateTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class UservalidateTest {
     @Test
-    public void FirstName_Check_istrue(){
+    public void FirstName_Check(){
         UserValue validate=new UserValue(null);
         try{
             validate.FirstName(null);
@@ -13,67 +13,40 @@ public class UservalidateTest {
             Assert.assertEquals("Please enter valid first name", e.getMessage());
         }
     }
-
-    /*@Test
-    public void FirstName_Check_isfalse(){
-        UserValue validate=new UserValue();
-        boolean result=validate.FirstName("mu");
-        Assert.assertEquals(false,result);
-    }
-
     @Test
-    public void LastName_Check_istrue(){
-        UserValue validate=new UserValue();
-        boolean result=validate.LastName("Hello");
-        Assert.assertEquals(true,result);
+    public void LastName_Check(){
+        UserValue validate=new UserValue(null);
+        try{
+            validate.LastName(null);
+        }catch(UserValueException e){
+            Assert.assertEquals("Please enter valid last name", e.getMessage());
+        }
     }
-
     @Test
-    public void LastName_Check_isfalse(){
-        UserValue validate=new UserValue();
-        boolean result=validate.LastName("he");
-        Assert.assertEquals(false,"he");
+    public void Email_Check(){
+        UserValue validate=new UserValue(null);
+        try{
+            validate.Email(null);
+        }catch(UserValueException e){
+            Assert.assertEquals("Please enter valid emailid", e.getMessage());
+        }
     }
-
     @Test
-    public void Email_Check_istrue(){
-        UserValue validate=new UserValue();
-        boolean result=validate.Email("murali@example.com");
-        Assert.assertEquals(true,result);
+    public void Mobile_Check(){
+        UserValue validate=new UserValue(null);
+        try{
+            validate.Mobile(null);
+        }catch (UserValueException e){
+            Assert.assertEquals("Please enter valid phone number", e.getMessage());
+        }
     }
-
     @Test
-    public void Email_Check_isfalse(){
-        UserValue validate=new UserValue();
-        boolean result=validate.Email(".asx");
-        Assert.assertEquals(false,result);
+    public void Password_Check(){
+        UserValue validate=new UserValue(null);
+        try{
+            validate.Password(null);
+        }catch (UserValueException e) {
+            Assert.assertEquals("Please enter valid password", e.getMessage());
+        }
     }
-
-    @Test
-    public void Mobile_Check_istrue(){
-        UserValue validate=new UserValue();
-        boolean result=validate.Mobile(" 91 9894119421");
-        Assert.assertEquals(true,result);
-    }
-
-    @Test
-    public void Mobile_Check_isfalse(){
-        UserValue validate=new UserValue();
-        boolean result=validate.Mobile("5793");
-        Assert.assertEquals(false,result);
-    }
-
-    @Test
-    public void Password_Check_istrue(){
-        UserValue validate=new UserValue();
-        boolean result=validate.Password("Muralis@18");
-        Assert.assertEquals(true,result);
-    }
-
-    @Test
-    public void Password_Check_isfalse(){
-        UserValue validate=new UserValue();
-        boolean result=validate.Password("345");
-        Assert.assertEquals(false,result);
-    }*/
 }
